a946d2aa5f19aee357cdb164d6f1ce2ad19d1b2e
NelsonChegeNjenga <chegenelson641@gmail.com>
2025-07-13 22:21:22 +0300

    first commit

--- Todo/models.py
+++ Todo/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Todo(models.Model):
-    name = models.CharField(max_length=200)
-    completed = models.CharField(default=False)
+    name = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.CharField(max_length=10, default="False")  # ✅ Fixed
+
 
     def __str__(self):
         return self.name
